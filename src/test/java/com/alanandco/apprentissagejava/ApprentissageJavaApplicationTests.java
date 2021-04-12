57e437182d21d5a0157ba34d7c57009ea0550cfc
Alan Duchene <alanduchene@MacBook-Pro-de-Alan.local>
2021-04-12 19:41:45 +0200

    feat(initialisation): Init maven project

---
+++ src/test/java/com/alanandco/apprentissagejava/ApprentissageJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanandco.apprentissagejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprentissageJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
